1be9e1c9c67c1735b2831e619b00f23269bbca25
Wardah Jabeen <jabeen@usc.edu>
2024-02-13 20:50:22 -0800

    fixed typo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/hero.jpg')] bg-cover bg-no-repeat">
             <div className="w-full lg:w-2/3 space-y-5">
-                <h1 className='text-backgoundColor font-semibold text-6xl'>Elevate your inner cafe with eveery bite! </h1>
+                <h1 className='text-backgoundColor font-semibold text-6xl'>Elevate your inner cafe with every bite! </h1>
                 <p className='text-brightColor'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, hic cum facilis quibusdam alias suscipit, labore perferendis saepe quae incidunt reiciendis doloribus deleniti ducimus accusamus! Rem ullam magnam ex odio.
                 </p>
                 <div className='lg:pl-44'>
